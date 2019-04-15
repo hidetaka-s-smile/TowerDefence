a4b979d36d322fad7b8447d49f5e8cc5c1ad14e8
cthxzy <49591316+cthxzy@users.noreply.github.com>
2019-04-15 23:49:50 +0800

    天恒
    第一代塔的预制体和脚本

---
+++ 熔火之心/Assets/Scripts/Tower scripts/tower1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tower1 :tower
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初始化
+        hp = 100;
+        attackRateTime = 0.2f;
+        ad = 10;
+
+    }
+
+    // Update is called once per frame
+    
+}
