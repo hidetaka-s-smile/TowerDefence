809bf337b1749d7e85b9c10ae121055643c62e92
VisionWong <49423256+VisionWong@users.noreply.github.com>
2019-04-16 10:49:16 +0800

    添加了我昨晚做的UI预制体和脚本
    -然后动画移动到Asset-》Animations下面吧，方便后面找，这个资源包给的文件夹太复杂了

---
+++ 熔火之心/Assets/Scripts/UI/BluePrintPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePrintPanel : MonoBehaviour
+{
+    private Animation anim;
+    private bool isOpen = false;
+
+    private void Awake()
+    {
+        anim = gameObject.GetComponent<Animation>();        
+    }
+
+    /// <summary>
+    /// 面板进入游戏界面
+    /// </summary>
+    public void OnPanelEnter()
+    {      
+        if(!isOpen && !anim.isPlaying)
+        {
+            isOpen = true;
+            anim.Play("BluePrintPanelEnter");
+        }
+    }
+
+    /// <summary>
+    /// 面板离开游戏界面
+    /// </summary>
+    public void OnPanelExit()
+    {       
+        if (isOpen && !anim.isPlaying)
+        {
+            isOpen = false;
+            anim.Play("BluePrintPanelExit");
+        }
+    }
+}
