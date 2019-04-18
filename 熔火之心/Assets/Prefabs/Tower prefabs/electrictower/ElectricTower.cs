d3520ffc3f830e76a756cfa76b88173be34edb6c
cthxzy <49591316+cthxzy@users.noreply.github.com>
2019-04-19 00:14:56 +0800

    天恒

---
+++ 熔火之心/Assets/Prefabs/Tower prefabs/electrictower/ElectricTower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricTower :Tower
+{
+    public override void Update()
+    {
+        //如果不在建造中
+        if (IsBuilding == false)
+        {
+            
+            timer += Time.deltaTime;
+            //攻击
+            if (enemys.Count > 0 && timer > attackRateTime)
+            {
+                timer = 0;
+                Attack();
+            }
+        }
+    }
+}
