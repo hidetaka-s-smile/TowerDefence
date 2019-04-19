c1df59a40b99e8f6e8027b2fc249b3f2c46a5458
wujunjie1008 <445404396@qq.com>
2019-04-19 22:45:34 +0800

    上传了熔炉资源

---
+++ 熔火之心/Assets/Scripts/burner/Burner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burner : MonoBehaviour
+{
+    public GameObject gear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Creat()
+    {
+        GameObject NewGear = GameObject.Instantiate(gear, new Vector3(transform.position.x, transform.position.y + 5f, transform.position.z + 5f), Quaternion.Euler(0.0f, 0.0f, 90.0f)) as GameObject;
+        NewGear.GetComponent<Gear>().num = 50;
+    }
+}
