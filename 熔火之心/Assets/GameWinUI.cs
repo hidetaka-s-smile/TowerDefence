1a3823c41e423150698c0dbadb22df525e2ee150
VisionWong <49423256+VisionWong@users.noreply.github.com>
2019-04-19 22:46:38 +0800

    胜利界面做完了

---
+++ 熔火之心/Assets/GameWinUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameWinUI : MonoBehaviour
+{
+    //单例模式
+    public static GameWinUI instance;
+
+    public Image frameImg;
+    public Animation frameAnim;
+    public Image victoryImg;
+    public Animation victoryAnim;
+    public Text grandPlayerTxt;
+    public Animation grandPlayerAnim;
+    public Text saveWorldTxt;
+    public Animation saveWorldAnim;
+    public GameObject endBtn;
+
+    public void Show()
+    {
+
+    }
+
+    private void ShowFrame()
+    {
+        frameImg.enabled = true;
+        
+    }
+}
