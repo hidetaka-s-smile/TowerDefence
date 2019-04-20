f19bb6c38182933d7b74b20962a38a857d350572
VisionWong <49423256+VisionWong@users.noreply.github.com>
2019-04-21 00:44:58 +0800

    初始界面Timeline

---
+++ 熔火之心/Assets/Scripts/UI/TitleUIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleUIManager : MonoBehaviour
+{
+    public AudioClip enterClip;
+    public Animation maskAnim;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void OnStartBtn()
+    {
+        audioSource.PlayOneShot(enterClip);
+        maskAnim.Play();
+        Invoke("LoadGame", 1.0f);
+    }
+
+    public void OnQuitBtn()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// 异步加载游戏
+    /// </summary>
+    private void LoadGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+
+}
